3c97fa2062788636765bf4172cd30809d2532b48
pop9310@gmail.com <pop9310@gmail.com@1f8b6fd5-fe88-54bb-c49a-f99ac05e70af>
2014-01-14 12:04:59 +0000

    테스트용 이미지 추가 - url연결.... 나중에 페이스북 멤버 id받으면 url던져주기만 하면 로드 할듯...
    UrlImageView 
    이미지 백그라운드에서 다운로드 받아 보여주기 기능
    
    메인 이미지에서 가로를 화면 가로에 맞췄는데 세로를 어떻게 해야할지 모르겠음.
    
    ! 상세보기 엑티비티에서 가로 스와이프하면 이전 화면으로 돌아가도록? (pinterest 화면 처럼....)

--- photify/photify/src/main/java/ac/jejunu/photify/DetailActivity.java
+++ photify/photify/src/main/java/ac/jejunu/photify/DetailActivity.java
@@ -2,6 +2,15 @@ package ac.jejunu.photify;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import ac.jejunu.photify.view.UrlImageView;
 
 public class DetailActivity extends Activity {
 
@@ -9,5 +18,32 @@ public class DetailActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail);
+
+		try {
+			UrlImageView mainImage = (UrlImageView) findViewById(R.id.iv_mainphoto);
+			mainImage.setImageURL(new URL("http://icon.daumcdn.net/w/icon/1312/19/152729032.png"));
+
+			for (int i = 0; i < 10; i++)
+				addComment("https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/user_male2-256.png", "name" + i, "text" + i);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 	}
+
+	private void addComment(String profilePicPath, String name, String contents) throws MalformedURLException {
+		LinearLayout commentsLayout = (LinearLayout) findViewById(R.id.ll_comments);
+		View child = getLayoutInflater().inflate(R.layout.profile_and_text, null);
+
+		UrlImageView profilePic = (UrlImageView) child.findViewById(R.id.iv_profilepic);
+		TextView tvName = (TextView) child.findViewById(R.id.tv_name);
+		TextView tvContent = (TextView) child.findViewById(R.id.tv_contents);
+
+		profilePic.setImageURL(new URL(profilePicPath));
+		tvName.setText(name);
+		tvContent.setText(contents);
+
+		commentsLayout.addView(child);
+	}
+
+
 }
